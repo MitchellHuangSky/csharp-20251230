11bf75f686d8a145f1a42cfc5c6a4babe0e083da
Taiming <Taiming@FT-PV038-F2-TAI>
2025-12-30 20:05:59 +0800

    加入專案檔案。

---
+++ WebApplication2/Models/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+
+namespace WebApplication2.Models.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
